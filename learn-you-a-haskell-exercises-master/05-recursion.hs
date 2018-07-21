a78d8d6fd5c4585d6ee2432d7bb3c40b65667bb3
burseylj <burseylj@users.noreply.github.com>
2018-07-21 18:26:39 -0400

    finished 5th chapter

--- learn-you-a-haskell-exercises-master/05-recursion.hs
+++ learn-you-a-haskell-exercises-master/05-recursion.hs
@@ -1,7 +1,48 @@
+-- my attempts at programs from the text (Louis)
+
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n x = x : replicate (n-1) x 
+
+take' :: (Num n, Ord n) => n -> [a] -> [a]
+take' n _
+    | n <= 0  = []
+take' _ [] = []
+take' n (x:xs) = x:take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse xs ++ [x] 
+
+repeat' :: x -> [x]
+repeat' x = x: repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip'  [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+elem' :: (Eq a) => a-> [a] -> Bool
+elem' _ [] = False
+elem' i (x:xs)
+    | i == x  = True
+    | otherwise = elem' i xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerSorted = quicksort [a | a<- xs, a<= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+
 -- Raise x to the power y, using recursion
 -- For example, power 5 2 = 25
 power :: Int -> Int -> Int
-power x y = undefined
+power x y 
+    | x == 0  || y <= 0 = undefined
+    | y == 0 = 1
+    | y > 0 = x * power x (y - 1)
 
 -- create a list of length n of the fibbonaci sequence in reverse order
 -- examples: fib 0 = [0]
@@ -9,7 +50,11 @@ power x y = undefined
 --	     fib 10 = [55,34,21,13,8,5,3,2,1,1,0]	
 -- try to use a where clause
 fib :: (Num a, Eq a) => a -> [a]
-fib x = undefined
+fib x 
+    | x == 0 = [0]
+    | x == 1 = [1,0]
+    | otherwise = (y + z) : all 
+    where all@(y:z:_) = fib (x-1)
 
 -- This is not recursive, but have a go anyway.
 -- Create a function which takes two parameters, a number and a step
@@ -18,7 +63,9 @@ fib x = undefined
 --			    stepReverseSign -3 1 = 4
 --			    stepReverseSign 1 2 = -3
 stepReverseSign :: (Fractional a, Ord a) => a -> a -> a
-stepReverseSign a = undefined
+stepReverseSign a
+    | a < 0 = (+) (abs a)
+    | otherwise = (-) (-a)
 
 {- Lets calculate pi.
  - The Leibniz formula for pi (http://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80)
@@ -52,8 +99,12 @@ stepReverseSign a = undefined
  -}
 
 piCalc :: (Fractional a, Integral b, Ord a) => a -> (a, b)
-piCalc a = undefined
+piCalc a = piCalc' 1 0 a 0
 
 piCalc' :: (Ord a, Fractional a, Integral b) => a -> a -> a -> b -> (a, b)
-piCalc' w x y z = undefined
+piCalc' denom val tol calls
+    | toAdd < tol = (val, calls)
+    |otherwise  =  piCalc' (denom + 2) (stepReverseSign (4 / denom) val) tol (calls + 1)
+    where toAdd = 4/denom
+          
 
